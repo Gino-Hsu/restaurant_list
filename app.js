ae414f0eb9f30c6bb935caeba187f3247b813a23
GinoHsu <ginohsu16@gmail.com>
2022-08-22 02:17:20 +0800

    Initialize project Create index page

---
+++ app.js
@@ -0,0 +1,20 @@
+// require packages used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+
+// setting template engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
